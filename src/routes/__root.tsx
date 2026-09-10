5a166f2326091535a5184eee881dbc387fa72846
illegalcaffeine <illegalcaffeine@gmail.com>
2026-09-11 03:39:05 +0900

    Mount goodbye overlay globally

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,6 +15,7 @@ import { SiteHeader } from "@/components/site/header";
 import { SiteFooter } from "@/components/site/footer";
 import { SiteExtras } from "@/components/site/site-extras";
 import { CursorFollower } from "@/components/site/cursor-follower";
+import { GoodbyeOverlay } from "@/components/site/goodbye-overlay";
 import { Toaster } from "@/components/ui/sonner";
 import { LanguageProvider, useLanguage } from "@/i18n";
 import { LanguageGate } from "@/components/site/language-gate";
@@ -114,6 +115,7 @@ function RootComponent() {
         <main><Outlet /></main>
         <SiteExtras />
         <SiteFooter />
+        <GoodbyeOverlay />
         <Toaster />
         <CursorFollower />
       </LanguageProvider>
